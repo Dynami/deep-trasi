1698c342eaba1ec0bdcad99baa43fb85fe3c8de7
Alessandro Atria <a.atria@gmail.com>
2019-03-17 23:20:34 +0100

    testing and minor improvements

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
     conn = sqlite3.connect('./data/trasi.db')
 
     df = prep.main(conn, look_fwd=10, threshold=0.015, option_step=250.0)
-    ms.main(df)
+    #ms.main(df)
 
 
 if __name__ == '__main__':
--- model_selection/model_selection.py
+++ model_selection/model_selection.py
@@ -8,6 +8,7 @@ warnings.filterwarnings("ignore")
 
 
 def split_dataset(df, split_ratio):
+    # df= df[::-1]
     n_train = int(df.shape[0] * split_ratio)
 
     x_train_data = df.iloc[:n_train, :-2]
@@ -26,16 +27,19 @@ def get_class_weight(y_train):
     max_count = pd.Series(y_labels).value_counts().max()
     return (pd.Series(y_labels).value_counts() / max_count).to_dict()
 
+
 def evaluate(clf, x_train, y_train, class_weight=None, cv=5, scoring='accuracy'):
-    if(class_weight is not None):
+
+    if class_weight is not None:
         model = clf['clf'](class_weight=class_weight, random_state=True)
     else:
         model = clf['clf']()
+
     print(type(model))
-    #np.random.seed(2888306641) # Best seed found
+    np.random.seed(2888306641)  # Best seed found
     seed = np.random.get_state()[1][0]
     print('np.random.seed', seed)
-    clf = GridSearchCV(model, clf['parameters'], cv=cv, scoring=scoring)
+    clf = GridSearchCV(model, clf['parameters'], cv=cv)
     clf.fit(x_train, y_train)
 
     print(clf.best_params_)
@@ -49,9 +53,9 @@ def evaluate(clf, x_train, y_train, class_weight=None, cv=5, scoring='accuracy')
 def scoring(model, x_test, y_test):
     y_pred = model.predict(x_test)
 
-    y_pred_proba = model.predict_proba(x_test)
-    y_pred_proba = [y_pred_proba[i, v] for i, v in enumerate(np.argmax(y_pred_proba, axis=1))]
-    y_pred_proba = np.array(y_pred_proba)
+    #y_pred_proba = model.predict_proba(x_test)
+    #y_pred_proba = [y_pred_proba[i, v] for i, v in enumerate(np.argmax(y_pred_proba, axis=1))]
+    #y_pred_proba = np.array(y_pred_proba)
 
     y_test_data_sparse = y_test[['sparse_target']].values
     y_test_data_sparse = np.reshape(y_test_data_sparse, (-1))
@@ -93,16 +97,16 @@ def main(df, split_ratio=0.80, models=None):
     x_train_data, x_test_data, y_train_data, y_test_data = split_dataset(df, split_ratio)
 
     class_weight = get_class_weight(y_train_data)
-
+    print('class_weight', class_weight)
     clfs = []
 
     for i, clf in enumerate(models):
         _class_weight = class_weight if clf['use_class_weight'] else None
         _clf = evaluate(clf, x_train=x_train_data.values, y_train=y_train_data[['sparse_target']].values, class_weight=_class_weight)
 
-        _result = scoring( _clf['model'], x_test_data, y_test_data)
+        _result = scoring(_clf['model'], x_test_data.values, y_test_data)
         _clf['results'] = _result
-        clfs.append( _clf )
+        clfs.append(_clf)
         print('-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-')
 
     print(clfs)
@@ -111,11 +115,14 @@ def main(df, split_ratio=0.80, models=None):
 
 if __name__ == '__main__':
     df = pd.read_csv('../trasi.csv', index_col='index')
-    from sklearn.linear_model import LogisticRegression
+    df.drop(columns='Unnamed: 0', inplace=True)
+    from sklearn.linear_model import LogisticRegression, Lasso, LassoLars, Lars
     from sklearn.svm import SVC
     from sklearn.tree import DecisionTreeClassifier
     from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier
     from sklearn.neural_network import MLPClassifier
+    from xgboost import XGBClassifier
+
     models = [
         # {
         #     'clf':LogisticRegression,
@@ -209,26 +216,55 @@ if __name__ == '__main__':
         #         'n_estimators': [50, 100]
         #     }
         #},
+        # {
+        #     # Total  >>>>> 70.821 %
+        #     # Prec.  >>>>> 64.557 %
+        #     # Cover. >>>>> 64.228 %
+        #     # Sharp  >>>>> 0.314
+        #     # Mean   >>>>> 0.009
+        #     # Std    >>>>> 0.029
+        #     'clf': MLPClassifier,
+        #     'use_class_weight': False,
+        #     'parameters': {
+        #         'shuffle': [True],
+        #         'hidden_layer_sizes':[(300, 100, 200)],
+        #         'activation': ['relu'],
+        #         #'nesterovs_momentum': [True, False],
+        #         'solver': ['adam'],
+        #         'alpha':[0.0001],
+        #         'learning_rate': ['invscaling'],# 'constant', 'adaptive',
+        #         #'early_stopping':[True],
+        #     }
+        # },
         {
-            # Total  >>>>> 70.821 %
-            # Prec.  >>>>> 64.557 %
-            # Cover. >>>>> 64.228 %
-            # Sharp  >>>>> 0.314
-            # Mean   >>>>> 0.009
-            # Std    >>>>> 0.029
-            'clf': MLPClassifier,
+            #Total >>>>> 263.927 %
+            #Prec. >>>>> 65.854 %
+            #Cover.>>>>> 100.000 %
+            #Sharp >>>>> 0.400
+            #Mean  >>>>> 0.021
+            #Std   >>>>> 0.054
+            'clf': Lasso,
             'use_class_weight': False,
             'parameters': {
-                'shuffle': [True],
-                'hidden_layer_sizes':[(300, 100, 200)],
-                'activation': ['relu'],
-                #'nesterovs_momentum': [True, False],
-                'solver': ['adam'],
-                'alpha':[0.0001],
-                'learning_rate': [ 'invscaling'],# 'constant', 'adaptive',
-                #'early_stopping':[True],
+                'alpha': [1.8], #np.arange(1.0, 2.2, 0.2),
+                'selection': ['cyclic']
             }
-        }
+        },
+        # {
+        #     # Total >>>>> -3.855 %
+        #     # Prec. >>>>> 52.273 %
+        #     # Cover.>>>>> 71.545 %
+        #     # Sharp >>>>> -0.012
+        #     # Mean  >>>>> -0.000
+        #     # Std   >>>>> 0.036
+        #     'clf': XGBClassifier,
+        #     'use_class_weight': False,
+        #     'parameters': {
+        #         'booster': ['gbtree', 'gblinear', 'dart'],
+        #         'reg_alpha': [0.1, 1.0, 2.],
+        #         'reg_lambda': [0.001]
+        #     }
+        # }
     ]
 
     main(df, models=models)
